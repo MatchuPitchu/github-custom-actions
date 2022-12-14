7805940ab96af963f3b0b4c4a0ecef3dd6f5b99f
MatchuPitchu <michael.flohr@posteo.de>
2022-12-14 12:18:24 +0100

    ci: finish custom js action [skip ci]

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -5,6 +5,21 @@ import * as exec from '@actions/exec';
 const run = () => {
   // prints message to workflow log
   core.notice('Hello from my custom JS action');
+
+  // 1) Get input values (defined in action.yml) to build a generic custom action
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true }); // true since default will be provided in case
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files
+  // exec can run shell command in js code
+  // here use AWS CLI -> it's pre-installed on ubuntu-latest runner machine
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  // 3) Generate output value
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // key is defined in action.yml
 };
 
 run();
